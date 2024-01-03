3dae4f10d7357697db7b76b7a5d9bb9808fec6b0
Rayssa Maria <rayssa_mcq@hotmail.com>
2024-01-03 19:25:41 -0300

    Desafio finalizado

--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("8080-8080", "Iphone 13", "2222222", 64);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("8008-8008", "Iphone 15", "2222542", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("Telegram");
